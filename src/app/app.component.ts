dab56eb7b1994c49c6695f1ab021d526ba061be8
Nick Clifton <nicholas.clifton@gmail.com>
2017-06-09 09:45:31 -0400

    Fullpage configured and working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import 'jquery';
+import 'fullpage.js'; // Import fullpage.js
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MaterialModule, MdNativeDateModule} from '@angular/material';
-import {MdTabsModule} from '@angular/material';
+import { MnFullpageModule } from 'ngx-fullpage';
 
 import { AppComponent } from './app.component';
-import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TabsComponent
-  ],
+      AppComponent,
+    ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
-    MaterialModule,
-    MdNativeDateModule,
-    ReactiveFormsModule,
+    MnFullpageModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
