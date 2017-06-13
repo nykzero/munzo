52947829c9d09aae7314629387b729ba21a49da8
CliftonN <CliftonN@JH5DQ92.corp.saic.com>
2017-06-12 23:35:59 -0400

    added fullpage.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, ViewEncapsulation } from '@angular/core';
+import { MnFullpageOptions, MnFullpageService } from 'ngx-fullpage';
 
 import 'jquery';
 import 'fullpage.js'; // Import fullpage.js
@@ -6,7 +7,7 @@ import 'fullpage.js'; // Import fullpage.js
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Munzo Solutions';
